70735c6f930fd63527636a27325fe1cf0770fda2
Hacanna42 <dtmdgus901@gmail.com>
2025-03-29 23:33:36 +0900

    chore: DB 외래키 설정으로 구조 개선

--- docker/init.sql
+++ docker/init.sql
@@ -1,17 +1,18 @@
 USE janggi;
-CREATE TABLE `janggi`.`game_session` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `current_turn` VARCHAR(45) NULL,
-    `status` VARCHAR(45) NULL,
-    PRIMARY KEY (`id`)
+CREATE TABLE game_session (
+      id INT NOT NULL AUTO_INCREMENT,
+      current_turn VARCHAR(45),
+      status VARCHAR(45),
+      PRIMARY KEY (id)
 );
 
-CREATE TABLE `janggi`.`piece_state` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `game_session_id` INT NOT NULL,
-    `type` VARCHAR(45) NOT NULL,
-    `team` VARCHAR(45) NOT NULL,
-    `position_row` INT NOT NULL,
-    `position_column` INT NOT NULL,
-    PRIMARY KEY (`id`)
+CREATE TABLE piece_state (
+     id INT NOT NULL AUTO_INCREMENT,
+     game_session_id INT NOT NULL,
+     type VARCHAR(45) NOT NULL,
+     team VARCHAR(45) NOT NULL,
+     position_row INT NOT NULL,
+     position_column INT NOT NULL,
+     PRIMARY KEY (id),
+     FOREIGN KEY (game_session_id) REFERENCES game_session(id) ON DELETE CASCADE
 );
